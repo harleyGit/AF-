22c4632bd5136489c3cbc5baf0af0af6ff2c573e
Harley <harelysoa@qq.com>
2020-05-27 16:09:48 +0800

    feat：SDWebImage3.5.3 源码添加

---
+++ AFNetworkInterpret/ThirdLibry/SDWebImage[5.7.3]/SDWebImage/Core/SDMemoryCache.m
@@ -0,0 +1,163 @@
+/*
+ * This file is part of the SDWebImage package.
+ * (c) Olivier Poitrey <rs@dailymotion.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+#import "SDMemoryCache.h"
+#import "SDImageCacheConfig.h"
+#import "UIImage+MemoryCacheCost.h"
+#import "SDInternalMacros.h"
+
+static void * SDMemoryCacheContext = &SDMemoryCacheContext;
+
+@interface SDMemoryCache <KeyType, ObjectType> ()
+
+@property (nonatomic, strong, nullable) SDImageCacheConfig *config;
+#if SD_UIKIT
+//弱引用缓存
+@property (nonatomic, strong, nonnull) NSMapTable<KeyType, ObjectType> *weakCache; // strong-weak cache
+//信号量的锁
+@property (nonatomic, strong, nonnull) dispatch_semaphore_t weakCacheLock; // a lock to keep the access to `weakCache` thread-safe
+#endif
+@end
+
+@implementation SDMemoryCache
+
+- (void)dealloc {
+    [_config removeObserver:self forKeyPath:NSStringFromSelector(@selector(maxMemoryCost)) context:SDMemoryCacheContext];
+    [_config removeObserver:self forKeyPath:NSStringFromSelector(@selector(maxMemoryCount)) context:SDMemoryCacheContext];
+#if SD_UIKIT
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
+#endif
+    self.delegate = nil;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _config = [[SDImageCacheConfig alloc] init];
+        [self commonInit];
+    }
+    return self;
+}
+
+- (instancetype)initWithConfig:(SDImageCacheConfig *)config {
+    self = [super init];
+    if (self) {
+        _config = config;
+        [self commonInit];
+    }
+    return self;
+}
+
+- (void)commonInit {
+    SDImageCacheConfig *config = self.config;
+    self.totalCostLimit = config.maxMemoryCost;
+    self.countLimit = config.maxMemoryCount;
+
+    [config addObserver:self forKeyPath:NSStringFromSelector(@selector(maxMemoryCost)) options:0 context:SDMemoryCacheContext];
+    [config addObserver:self forKeyPath:NSStringFromSelector(@selector(maxMemoryCount)) options:0 context:SDMemoryCacheContext];
+
+#if SD_UIKIT
+    //weakCache是一个键是强引用，值是弱引用的MapTable
+    self.weakCache = [[NSMapTable alloc] initWithKeyOptions:NSPointerFunctionsStrongMemory valueOptions:NSPointerFunctionsWeakMemory capacity:0];
+    self.weakCacheLock = dispatch_semaphore_create(1);
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didReceiveMemoryWarning:)
+                                                 name:UIApplicationDidReceiveMemoryWarningNotification
+                                               object:nil];
+#endif
+}
+
+// Current this seems no use on macOS (macOS use virtual memory and do not clear cache when memory warning). So we only override on iOS/tvOS platform.
+#if SD_UIKIT
+//在收到内存警告的时候，仅仅清除了父类的对象，并没有清除weakCache的对象，因为是弱引用类型，也不用手动清除
+- (void)didReceiveMemoryWarning:(NSNotification *)notification {
+    // Only remove cache, but keep weak cache
+    //移除父类的对象
+    [super removeAllObjects];
+}
+
+// `setObject:forKey:` just call this with 0 cost. Override this is enough
+//setObject的相关方法都会调到这里来，因此只需重写这个方法
+- (void)setObject:(id)obj forKey:(id)key cost:(NSUInteger)g {
+    [super setObject:obj forKey:key cost:g];
+    if (!self.config.shouldUseWeakMemoryCache) {
+        return;
+    }
+    if (key && obj) {
+        // Store weak cache
+        //宏定义使用的是 dispatch_semaphore_wait，使其信号量减1
+        SD_LOCK(self.weakCacheLock);
+        [self.weakCache setObject:obj forKey:key];
+        //宏定义使用的是dispatch_semphore_signal  信号量加1
+        SD_UNLOCK(self.weakCacheLock);
+    }
+}
+
+- (id)objectForKey:(id)key {
+    id obj = [super objectForKey:key];
+    if (!self.config.shouldUseWeakMemoryCache) {
+        return obj;
+    }
+    if (key && !obj) {
+        // Check weak cache
+        SD_LOCK(self.weakCacheLock);
+        obj = [self.weakCache objectForKey:key];
+        SD_UNLOCK(self.weakCacheLock);
+        if (obj) {
+            // Sync cache
+            NSUInteger cost = 0;
+            if ([obj isKindOfClass:[UIImage class]]) {
+                cost = [(UIImage *)obj sd_memoryCost];
+            }
+            [super setObject:obj forKey:key cost:cost];
+        }
+    }
+    return obj;
+}
+
+- (void)removeObjectForKey:(id)key {
+    [super removeObjectForKey:key];
+    if (!self.config.shouldUseWeakMemoryCache) {
+        return;
+    }
+    if (key) {
+        // Remove weak cache
+        SD_LOCK(self.weakCacheLock);
+        [self.weakCache removeObjectForKey:key];
+        SD_UNLOCK(self.weakCacheLock);
+    }
+}
+
+- (void)removeAllObjects {
+    [super removeAllObjects];
+    if (!self.config.shouldUseWeakMemoryCache) {
+        return;
+    }
+    // Manually remove should also remove weak cache
+    SD_LOCK(self.weakCacheLock);
+    [self.weakCache removeAllObjects];
+    SD_UNLOCK(self.weakCacheLock);
+}
+#endif
+
+#pragma mark - KVO
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    if (context == SDMemoryCacheContext) {
+        if ([keyPath isEqualToString:NSStringFromSelector(@selector(maxMemoryCost))]) {
+            self.totalCostLimit = self.config.maxMemoryCost;
+        } else if ([keyPath isEqualToString:NSStringFromSelector(@selector(maxMemoryCount))]) {
+            self.countLimit = self.config.maxMemoryCount;
+        }
+    } else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
+@end
